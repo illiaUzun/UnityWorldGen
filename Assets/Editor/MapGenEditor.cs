c9804c07622f04772483d3a77801b7e50ea82069
Илья Узун <uzun.illia.main@gmail.com>
2019-01-19 21:06:35 +0200

    Noise Map

---
+++ Assets/Editor/MapGenEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGEN))]
+public class MapGenEditor : Editor {
+    public override void OnInspectorGUI() {
+        MapGEN mapGen = (MapGEN)target;
+
+        if (DrawDefaultInspector() && mapGen.autoUpdate) {
+            mapGen.Generate();
+        }
+        if (GUILayout.Button("Generate new map")) {
+            mapGen.Generate();
+        }
+    }
+}
---
+++ Assets/MapGEN.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGEN : MonoBehaviour {
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public bool autoUpdate;
+
+    public void Generate() {
+        float[,] noiseMap = PerlinNoise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
